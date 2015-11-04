ba726c1df74c8bf8b88b81fcd0969946de793be7
Paul Boyle <pboyle17@gmail.com>
2015-11-04 14:06:09 -0600

    lunchtime commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,188 @@
 window.onload=function(){
   console.log('javascript is working');
+  console.log('all files that this page needs has been loaded');
   var btn=document.getElementById('button');
   btn.onclick = function () {
     return alert('button is working!');
+  }//end of btn
+
+
+//declare a selector called maincontent
+//   var maincontent=document.getElementById('maincontent');
+//
+//   console.log(maincontent);
+//
+//   var monsters=['Wreck it Ralph','The giraffe from lion king snes','Gannon'];
+//
+//   for (var baddie in monsters){
+//     var li = document.createElement('li')
+//     console.log(li);
+//     //create a new dom element using document.createElement('name-of-tag')
+//     li.innerHTML = monsters[baddie];
+//     //aapend to container usings selector.appendChild(domElement);
+//     maincontent.appendChild(li);
+//   } //end of for-in loop
+//
+//  //now , we need to create an imgage
+//  //var kittenImage=document.createElement('img');
+//  //alt text - ADA compliancy text for the blind
+//  //kittenImage.alt='a cute random kitten';
+//  //src=image srouce
+//  //kittenImage.src='http://images4.fanpop.com/image/photos/16100000/Cute-Kitten-kittens-16122946-1280-800.jpg';
+//
+//  //append my element as a child to a selector
+// // maincontent.appendChild(kittenImage);
+//
+//  //modify the cuteness level of the kitten
+//  //kittenImage.id='kitten';
+// // var kitten = document.querySelector('#kitten')
+//
+//  //kitten.src='http://www.deshow.net/d/file/animal/2009-07/cute-kitten-631-2.jpg'
+//
+// // var listItemsArray = document.getElementsByTagName('li');
+//  //console.log(listItemsArray);
+//
+//  //for (var li in listItemsArray){
+//    //listItemsArray[li].innerHTML = 'We are the champions (my friend)';
+//  //}
+//
+//  var status=document.getElementById('status-message');
+// var btn=document.getElementById('addPoint')
+//
+// //intialize UI component
+// user.initialize(status);
+// btn.addEventListener('click',function(){
+//   user.updateScoreByOnePoint();
+// });
+//
+// // //must wrap in ana. function otherwise will only run once on first click
+// // user.initialize(status);
+// // btn.addEventListener('click',user.updateScoreByOnePoint());
+//
+//
+//
+//  //bind event to a DOM element
+//  //var body = document.getElementsByTagName('body')[0];
+//  //mouse events
+//  //body.addEventListener('click', function() {
+//    //console.log('oww dont taze me bro!');
+//  //});
+//  //touchstart
+//  //touchmove
+//  //touchend
+//  //console.log(event)
+//  //touch events
+// //body.addEventListener('touchstart',function(event){
+//   //console.log('yoyoyo y you poking me bro? wtf!');
+// //});
+// //keyboard event
+// //body.addEventListener('keyup',function(event){
+//   // look for specific keys to be pressed
+//   //if(event.keyCode == 13){
+//     //console.log('why you press enter so much yo')
+//   //}
+//   //console.log(event.keyCode);
+//
+//
+// //})
+//
+//
+//
+//
+//
+}//end of window.onload function
+//
+// //mouse events stored in an array, how to remove an event listener
+//
+// //create an object that can update itself and visually show that if needed.
+//
+// //ex. 1: user component
+//
+// var user = {
+//   name: null ,
+//   score: 0,
+//   domElement: null,
+//
+//   initialize:function(elementToAppendTo){
+//     if(this.name==null) {
+//       this.name=prompt('What is your name?')
+//     }
+//     this.domElement=document.createElement('div');
+//     elementToAppendTo.appendChild(this.domElement);
+//
+//     console.log('initialize: complete');
+//   },
+//
+//   render:function(innerHTML){
+//     if(typeof(innerHTML)=='string'){
+//       this.domElement.innerHTML = innerHTML;
+//     }
+//   },
+// //valid html to place into our dom element
+//   getName:function(){
+//     return this.name
+//
+//   },
+//
+// //setter or mutators setting an interal value update a value
+//   saveName:function(newName){
+//     if (typeof(newName)=='string'&&newName.length>0){
+//       this.name=newName;
+//     } else {
+//       alert('you entered an incorrect or empty name');
+//     }
+//   },
+//
+//   getScore:function() {
+//     return this.score
+//   },
+//
+//   updateScoreByOnePoint: function(){
+//     this.score++;
+//     var status=this.buildPlayerStatusString();
+//     this.render(status);
+//     console.log(this.score);
+//     return this.score;
+//   },
+//
+//   buildPlayerStatusString:function() {
+//     return this.name + ': '+this.score;
+//   }
+//
+//
+//
+// } //end of user object
+//
+//
+//
+// //boiler-plate for a user interface component
+// var component = {
+//   initialize:function(){
+//     //create a DOM element
+//     //attach it
+//     this.domElement=document.createElement('div');
+//     selector.appendChild(this.domElement);
+//
+//   },
+//   render:function(statusText){
+//     //update the DOM element
+//     this.domElement.innerHTML=statusText;
+//   }
+// };
+
+card = {
+  name:'Eldrazi Skyspawner',
+  color:'Blue',
+  set:'BFZ',
+
+  intialize:function(){
+    console.log('print card to screen');
+  } ,
+
+  printCardName:function(){
+    console.log(this.name);
+    return this.name
   }
-}
+
+
+}// end of card object
