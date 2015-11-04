1db55e1622c61b3ea8c31112af3e927df3a4d8d7
Paul Boyle <pboyle17@gmail.com>
2015-11-04 08:13:45 -0600

    morning train work

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+window.onload=function(){
+  console.log('javascript is working');
+  var btn=document.getElementById('button');
+
+
+
+  btn.onclick = function () {
+    return alert('button is working!');
+  }
+}
