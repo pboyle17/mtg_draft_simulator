dc22d9eddcbde073fc69c92cede4494a5329b7b2
Paul Boyle <pboyle17@gmail.com>
2015-11-04 08:14:37 -0600

    updating

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,6 @@
 window.onload=function(){
   console.log('javascript is working');
   var btn=document.getElementById('button');
-
-
-
   btn.onclick = function () {
     return alert('button is working!');
   }
